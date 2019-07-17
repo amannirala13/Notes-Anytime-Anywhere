e90efac814247488e08fdb87341e364be3c918c5
amannirala13 <amansofttechinfo@gmail.com>
2019-07-17 19:30:44 +0530

    Added register screen

--- app/src/main/java/com/asdev/naa/MainActivity.java
+++ app/src/main/java/com/asdev/naa/MainActivity.java
@@ -1,16 +1,54 @@
 package com.asdev.naa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase db;
+    private DatabaseReference userdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-                
+                checkRegisteredUser();
 
+    }
+
+    private void checkRegisteredUser() {
+
+        db = FirebaseDatabase.getInstance();
+        userdb = db.getReference("Student").child("User");
+        userdb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.hasChild(FirebaseAuth.getInstance().getUid()))
+                    openRegisterActivity();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Ruh! Unable to connect to database", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void openRegisterActivity() {
+            Intent mainActivityIntent = new Intent(MainActivity.this, Register.class);
+            startActivity(mainActivityIntent);
+            finish();
     }
 }
